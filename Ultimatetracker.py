d5132820cf8991de3afe06c7e2a5e961adee9b90
mrmahan90 <43997733+mrmahan90@users.noreply.github.com>
2020-05-22 17:41:45 +0200

    Update Ultimatetracker.py

--- Ultimatetracker.py
+++ Ultimatetracker.py
@@ -1,1 +1,42 @@
+##### MAIN MENU #####
+
+### Försöker ärva från budget
+class UltimateTracker(Buget):
+  
+  def __init__(self):
+        self.mainMenu()
+    
+    
+    
+  def mainMenu(self):
+
+        print("\n****** WELCOME TO ULTIMATE TRACKER ******\n")
+        print("WHAT DO YOU WANT TO DAY?:")
+
+        try:
+
+            print("1 = SET BUDGET")
+            print("2 = SET TRANSACTION")
+            print("3 = SEE HISTORY")
+            print("4 = EXIT")
+
+            selection=int(input("Enter 1-4: "))
+
+            if selection==1:
+                self.setBudget()
+            elif selection==2:
+                self.setTransaction()
+            elif selection==3:
+                self.seeHistory()
+            elif selection==4:
+                exit
+            else:
+                print("***INVALID***")
+                print("Try again!")
+                self.mainMenu()
+        except:
+            print("Write a number from 1-6 \n")
+            self.mainMenu()
+
+
 
